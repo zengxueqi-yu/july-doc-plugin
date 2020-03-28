b555ed6ee62a5902405eae1a1d1e79f70fb85da9
zengxueqi-yu@outlook.com <zengxueqi-yu@outlook.com>
2020-03-29 00:14:29 +0800

    支持showdoc文档上传

--- src/main/java/com/july/doc/mojo/MarkdownMojo.java
+++ src/main/java/com/july/doc/mojo/MarkdownMojo.java
@@ -2,10 +2,15 @@ package com.july.doc.mojo;
 
 import com.july.doc.JulyDoc;
 import com.july.doc.entity.ApiConfig;
+import com.july.doc.showdoc.JulyShowDocUtil;
+import com.july.doc.showdoc.ShowDoc;
+import com.july.doc.showdoc.ShowDocModel;
 import com.thoughtworks.qdox.JavaProjectBuilder;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+
+import java.util.List;
 
 /**
  * 生成markdown文档信息
@@ -20,7 +25,13 @@ public class MarkdownMojo extends BaseMojo {
     @Override
     public void executeMojo(ApiConfig apiConfig, JavaProjectBuilder javaProjectBuilder) {
         try {
-            JulyDoc.generateDocApiPlugin(apiConfig,javaProjectBuilder);
+            List<ShowDocModel> showDocModelList = JulyDoc.generateDocApiPlugin(apiConfig,javaProjectBuilder);
+            //上传markdown文档至show doc
+            if(apiConfig.getShowDoc() != null){
+                ShowDoc showDoc = apiConfig.getShowDoc();
+                JulyShowDocUtil.doPost(new ShowDoc(apiConfig.getIsOpenLocal(), showDoc.getShowDocUrl(),
+                        showDoc.getApiKey(), showDoc.getApiToken(), showDocModelList));
+            }
         } catch (Exception e) {
             getLog().error(e);
         }
